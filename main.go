fd979bd15fd8eb288ed9fe48c16e4d56ebd2a481
grzegorzpnk <grzegorz.panek@orange.com>
2022-04-15 19:46:41 +0200

    final version

--- main.go
+++ main.go
@@ -1,8 +1,58 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
 func main() {
+
+	var animals map[string]Animal
+	animals = make(map[string]Animal)
 
 	bird := Animal{"worms", "fly", "peep"}
 	cow := Animal{"grass", "walk", "moo"}
-	snak := Animal{"mice", "slither", "hsss"}
+	snake := Animal{"mice", "slither", "hsss"}
+
+	animals["bird"] = bird
+	animals["cow"] = cow
+	animals["snake"] = snake
+
+	fmt.Println(animals)
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Println("please provide:  animal_name space request_type ")
+		text, _ := reader.ReadString('\n')
+		splitedText := strings.Fields(text)
+		fmt.Println(splitedText)
+		switch splitedText[1] {
+		case "food":
+			fmt.Println(animals[splitedText[0]].Eat())
+		case "locomotion":
+			fmt.Println(animals[splitedText[0]].Move())
+		case "noise":
+			fmt.Println(animals[splitedText[0]].Speak())
+		}
+	}
+}
+
+type Animal struct {
+	Food       string
+	Locomotion string
+	Noise      string
+}
+
+func (animal Animal) Eat() string {
+	return animal.Food
+}
+
+func (animal Animal) Move() string {
+	return animal.Locomotion
+}
+
+func (animal Animal) Speak() string {
+	return animal.Noise
 }
